5a5e8978fb6ceb39877e47cac2c32618d8bd6a0e
Jonathan El-Bizri <github@elbizri.com>
2016-04-25 17:54:44 -0700

    create: initial js, css and html files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "babel-polyfill"
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
+
+require('./style.css')
+
+class HelloWorld extends Component{
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World.</h1>
+        A few last reminders before you are on your way.
+        <ul>
+          <li>
+            Run 'npm run build' before uploading you public directory to your website.
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+const content = document.getElementById('app')
+
+ReactDom.render(<HelloWorld />, content)
