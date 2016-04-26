881e1338acf7e7d8f3f305f2543e337391d268a8
Jonathan El-Bizri <github@elbizri.com>
2016-04-25 19:33:51 -0700

    update: add Router, extract HelloWorld component
    Use router, pass in data and extract to containers
    
    index.js no longer renders the page component directly, but renders a hash based router. This router serves up the page Components from the `container` directory.

--- src/index.js
+++ src/index.js
@@ -6,23 +6,6 @@ import data from './data.js'
 
 import Router from './router.js'
 
-<<<<<<< HEAD
-/*
-  Call this function every time the data payload changes.
-
-  Or, if your site is truly static, just pass in the data you want to display.
-  Or, if you don't need to render data, ask yourself why you are using a framework,
-    and then delete the import statment above and the reference to it in the react
-    component render below.
-*/
-function onDataChange() {
-  // Splits location into `/` separated parts,
-  const location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
-  // Gets the DOM Element to render to
-  const content = document.getElementById('app')
-  // Renders with the location and the data
-  ReactDom.render(<Router {...data} location={location}/>, content)
-=======
   render() {
     return (
       <div>
@@ -36,7 +19,6 @@ function onDataChange() {
       </div>
     )
   }
->>>>>>> b98fa99... create README and update welcome reminder
 }
 
 window.addEventListener('hashchange', onDataChange, false);
