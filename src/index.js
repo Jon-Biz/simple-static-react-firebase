7acc0e2484e63fd302a0bd1189d3522c033d3320
Jonathan El-Bizri <github@elbizri.com>
2016-07-09 15:24:55 -0700

    add firebase data and auth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "babel-polyfill"
 import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 
-import data from './data.js'
+import Data from './data.js'
 
 import Router from './router.js'
 
@@ -14,14 +14,21 @@ import Router from './router.js'
     and then delete the import statment above and the reference to it in the react
     component render below.
 */
-function onDataChange() {
+const onDataChange = (data) => {
+  console.log('on data change')
+  console.log(data)
+
   // Splits location into `/` separated parts,
   const location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
   // Gets the DOM Element to render to
   const content = document.getElementById('app')
   // Renders with the location and the data
-  ReactDom.render(<Router {...data} location={location}/>, content)
+  const user = datum.user
+  if(Boolean(user)) ReactDom.render(<Router {...data} user={user} location={location}/>, content)
+  else ReactDom.render(<Router {...data} user={user} location={location}/>, content)
 }
+
+const datum = new Data(onDataChange)
 
 window.addEventListener('hashchange', onDataChange, false);
 
