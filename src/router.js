92e91df43bbc860f1f9c2e7f26f113f4e4e6a8c0
washt <washt23@gmail.com>
2016-07-11 08:54:53 -0400

    mispelled components

--- src/router.js
+++ src/router.js
@@ -1,12 +1,7 @@
 import React, {Component} from 'react'
 
-<<<<<<< HEAD
 import Helloworld from './containers/HelloWorld.js'
 import HelloAgainworld from './containers/HelloAgainWorld.js'
-=======
-import Helloworld from './containers/helloworld.js'
-import HelloAgainworld from './containers/helloAgainWorld.js'
->>>>>>> 5e7ba70... update: add Router, extract HelloWorld component
 
 class Router extends Component {
   render() {
